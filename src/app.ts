0409efcb3a4009ab6544f9a87bf860af97152121
Raigato <gabriel.raymondou@swile.co>
2022-03-25 16:45:27 +0100

    feat: healthz route

--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,8 @@ const app = express()
 
 app.use(express.json())
 
-app.get('/', (_, res) => {
-  res.send('Hello World!')
+app.get('/healthz', (_, res) => {
+  res.send('OK')
 })
 
 export default app
