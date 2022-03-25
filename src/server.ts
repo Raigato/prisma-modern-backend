301a0f9fd0d341c9af7dd9981b118af8e759d942
Raigato <gabriel.raymondou@swile.co>
2022-03-25 17:19:48 +0100

    feat: prisma disconnect on closing server

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import app from './app'
 import serverConfig from './config/server'
+import prisma from './lib/prisma'
 
 const { port, host } = serverConfig
 
@@ -7,12 +8,14 @@ const server = app.listen(port as number, host, () => {
   console.info(`Server listening on ${host}:${port}`)
 })
 
-process.on('unhandledRejection', (err) => {
+process.on('unhandledRejection', async (err) => {
   console.error('UnhandledRejection: Shutting down...', err)
+  await prisma.$disconnect()
   server.close(() => process.exit(1))
 })
 
-process.on('SIGTERM', () => {
+process.on('SIGTERM', async () => {
   console.info('SIGTERM received: Shutting down gracefully')
+  await prisma.$disconnect()
   server.close(() => console.info('Process terminated'))
 })
