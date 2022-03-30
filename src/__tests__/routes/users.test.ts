54acee754c2c14af7195f562fcb4ddd6272fe90a
Raigato <gabriel.raymondou@swile.co>
2022-03-30 13:26:27 +0200

    test: get single user tests

--- src/__tests__/routes/users.test.ts
+++ src/__tests__/routes/users.test.ts
@@ -17,7 +17,7 @@ describe('Users routes', () => {
   })
 
   describe('create user', () => {
-    it('should create returns a 201 with an user id', async () => {
+    it('should returns a 201 with an user id', async () => {
       const data = {
         email: 'gab@test.com',
         firstName: 'Gab',
@@ -44,8 +44,46 @@ describe('Users routes', () => {
     })
   })
 
+  describe('get single user', () => {
+    it('should return a 200 and the right user', async () => {
+      const data = {
+        email: 'singleUser@test.com',
+        firstName: 'Single',
+        lastName: 'User',
+      }
+
+      const user = await prisma.user.create({ data })
+
+      const response = await supertest(app).get(`/api/users/${user.id}`)
+
+      expect(response.statusCode).toEqual(200)
+
+      const { id, ...userData } = user
+
+      expect(response.body.data).toEqual(userData)
+    })
+
+    it('should return a 404 if user is not found', async () => {
+      const data = {
+        email: 'notFoundUser@test.com',
+        firstName: 'NotFound',
+        lastName: 'User',
+      }
+
+      const user = await prisma.user.create({ data })
+
+      const { id } = user
+
+      await prisma.user.delete({ where: { id } })
+
+      const response = await supertest(app).get(`/api/users/${user.id}`)
+
+      expect(response.statusCode).toEqual(404)
+    })
+  })
+
   describe('delete user', () => {
-    it('should delete an user successfully', async () => {
+    it('should return 204 after deleting an user successfully', async () => {
       const response = await supertest(app).delete(`/api/users/${userId}`)
 
       expect(response.statusCode).toEqual(204)
